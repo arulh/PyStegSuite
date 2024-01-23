63094435f2ca7faf5c10bdbfabf036cf861ba4ec
arulh <arul.howard@telus.net>
2024-01-23 14:27:11 -0800

    test

--- src/pss_image/helper.py
+++ src/pss_image/helper.py
@@ -1,31 +1,51 @@
 import random
 
-def encode_pixel(pixel: tuple, key: int) -> tuple:
-    """
-    Encodes the key into the lower order bits of the pixels RGB values.
+# def encode_pixel(pixel: tuple, key: int) -> tuple:
+#     """
+#     Encodes the key into the lower order bits of the pixels RGB values.
 
-    Parameters: 
-    Returns: Encoded pixel as tuple.
+#     Parameters:
+#     Returns: Encoded pixel as tuple.
+#     """
+
+#     key = key%64
+
+#     # extract RGB values from pixel
+#     r, g, b = pixel[0], pixel[1], pixel[2]
+
+#     # convert key to a binary string
+#     binary_key = bin(key)[2:].zfill(6)
+
+#     # encode the lower order bits of the RGB values with the key
+#     encoded_r = (r & 0b1111111100) | int(binary_key[0:2], 2)
+#     encoded_g = (g & 0b1111111100) | int(binary_key[2:4], 2)
+#     encoded_b = (b & 0b1111111100) | int(binary_key[4:6], 2)
+
+#     return (encoded_r, encoded_g, encoded_b, 255)
+
+def is_encoded_pixel(pixel: tuple, key: int) -> bool:
+    """
+    Parameters: pixel is from encoded image
+    Returns: true if pixels is encoded using that key and false otherwise
     """
 
     key = key%64
 
-    # extract RGB values from pixel
-    r, g, b = pixel[0], pixel[1], pixel[2]
-
     # convert key to a binary string
     binary_key = bin(key)[2:].zfill(6)
 
-    # encode the lower order bits of the RGB values with the key
-    r = (r & 0b1111111100) | int(binary_key[0:2], 2)
-    g = (g & 0b1111111100) | int(binary_key[2:4], 2)
-    b = (b & 0b1111111100) | int(binary_key[4:6], 2)
+    # extract RGB values from potential encoded pixel
+    r, g, b = pixel[0], pixel[1], pixel[2]
 
-    return (r, g, b, 255)
+    r = str((r & 0b1111111100) | int(binary_key[0:2], 2))
+    g = str((g & 0b1111111100) | int(binary_key[2:4], 2))
+    b = str((b & 0b1111111100) | int(binary_key[4:6], 2))
 
-def generate_key() -> int:
-    """
-    Returns:
-        Generated 4 digit key.
-    """
-    return random.randint(1000, 9999)
+    return binary_key[4:6] == r and binary_key[2:4] == g and binary_key[0:2] == b
+
+# def generate_key() -> int:
+#     """
+#     Returns:
+#         Generated 4 digit key.
+#     """
+#     return random.randint(1000, 9999)
