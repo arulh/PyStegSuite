8cfcc936cc0b984e45c183ea65426e42dcd5957e
arulh <arul.howard@telus.net>
2024-01-23 15:58:57 -0800

    added test files

--- src/pss_image/helper.py
+++ src/pss_image/helper.py
@@ -1,27 +1,27 @@
 import random
 
-# def encode_pixel(pixel: tuple, key: int) -> tuple:
-#     """
-#     Encodes the key into the lower order bits of the pixels RGB values.
+def encode_pixel(pixel: tuple, key: int) -> tuple:
+    """
+    Encodes the key into the lower order bits of the pixels RGB values.
 
-#     Parameters:
-#     Returns: Encoded pixel as tuple.
-#     """
+    Parameters:
+    Returns: Encoded pixel as tuple.
+    """
 
-#     key = key%64
+    key = key%64
 
-#     # extract RGB values from pixel
-#     r, g, b = pixel[0], pixel[1], pixel[2]
+    # extract RGB values from pixel
+    r, g, b = pixel[0], pixel[1], pixel[2]
 
-#     # convert key to a binary string
-#     binary_key = bin(key)[2:].zfill(6)
+    # convert key to a binary string
+    binary_key = bin(key)[2:].zfill(6)
 
-#     # encode the lower order bits of the RGB values with the key
-#     encoded_r = (r & 0b1111111100) | int(binary_key[0:2], 2)
-#     encoded_g = (g & 0b1111111100) | int(binary_key[2:4], 2)
-#     encoded_b = (b & 0b1111111100) | int(binary_key[4:6], 2)
+    # encode the lower order bits of the RGB values with the key
+    encoded_r = (r & 0b1111111100) | int(binary_key[0:2], 2)
+    encoded_g = (g & 0b1111111100) | int(binary_key[2:4], 2)
+    encoded_b = (b & 0b1111111100) | int(binary_key[4:6], 2)
 
-#     return (encoded_r, encoded_g, encoded_b, 255)
+    return (encoded_r, encoded_g, encoded_b, 255)
 
 def is_encoded_pixel(pixel: tuple, key: int) -> bool:
     """
@@ -37,15 +37,23 @@ def is_encoded_pixel(pixel: tuple, key: int) -> bool:
     # extract RGB values from potential encoded pixel
     r, g, b = pixel[0], pixel[1], pixel[2]
 
-    r = str((r & 0b1111111100) | int(binary_key[0:2], 2))
+    r = str(bin((r & 0b1111111100) | int(binary_key[0:2], 2)))
     g = str((g & 0b1111111100) | int(binary_key[2:4], 2))
     b = str((b & 0b1111111100) | int(binary_key[4:6], 2))
 
-    return binary_key[4:6] == r and binary_key[2:4] == g and binary_key[0:2] == b
+    # print(binary_key[0:2])
+    # print(r)
 
-# def generate_key() -> int:
-#     """
-#     Returns:
-#         Generated 4 digit key.
-#     """
-#     return random.randint(1000, 9999)
+    return binary_key[0:2] == r and binary_key[2:4] == g and binary_key[4:6] == b
+
+def generate_key() -> int:
+    """
+    Returns:
+        Generated 4 digit key.
+    """
+    return random.randint(1000, 9999)
+
+
+# pixel = (234, 50, 168, 255)
+# new_pixel = encode_pixel(pixel, 8888)
+# is_encoded_pixel(new_pixel, 8888)
---
+++ tests/test_pss_image.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append('../src/')
+
+import random
+import timeit
+from pss_image.helper import encode_pixel, is_encoded_pixel, generate_key
+
+def test_helper() -> None:
+    """
+    Tests all functions contained in helper.py
+    """
+
+    test_count = 100
+
+    for x in range(test_count):
+        test_key = generate_key()
+        pixel = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), 255)
+        new_pixel = encode_pixel(pixel, test_key)
+        try:
+            assert is_encoded_pixel(new_pixel, test_key)
+        except AssertionError:
+            print("\x1b[5;30;41m" + "TEST CASE FAILED" + "\x1b[0m")
+            sys.exit()
+
+
+if __name__ == "__main__":
+    start = timeit.default_timer()
+
+
+    test_helper()
+
+
+    stop = timeit.default_timer()
+    print("\x1b[6;30;42m" + "ALL TESTS PASSED" + "\x1b[0m")
+    print(f"Execution time: {(stop - start):.3f} seconds")
