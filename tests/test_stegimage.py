78b7e8716ef079299dd93c3fdf5c0055edad7470
arulh <arul.howard@telus.net>
2024-01-28 21:48:01 -0800

    added pytest framework

--- tests/test_stegimage.py
+++ tests/test_stegimage.py
@@ -16,20 +16,5 @@ def test_helper() -> None:
         test_key = generate_key()
         pixel = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), 255)
         new_pixel = encode_pixel(pixel, test_key)
-        try:
-            assert is_encoded_pixel(new_pixel, test_key)
-        except AssertionError:
-            print("\x1b[5;30;41m" + "TEST CASE FAILED" + "\x1b[0m")
-            sys.exit()
 
-
-if __name__ == "__main__":
-    start = timeit.default_timer()
-
-
-    test_helper()
-
-
-    stop = timeit.default_timer()
-    print("\x1b[6;30;42m" + "ALL TESTS PASSED" + "\x1b[0m")
-    print(f"Execution time: {(stop - start):.3f} seconds")
+        assert is_encoded_pixel(new_pixel, test_key)
