af81a440c3f31b1d9c4edf1aa21d3d5d87cf90af
arulh <arul.howard@telus.net>
2024-02-01 21:36:20 -0800

    implemented encode_stencil

--- src/stegimage/encoder.py
+++ src/stegimage/encoder.py
@@ -1,7 +1,29 @@
-from PIL import Image, ImageDraw, ImageFont 
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
 
-def encode_stencil(img_path: str, text: str, text_size=75, text_coords=(50, 50)) -> None:
+from helper import encode_pixel, generate_key
+
+def encode_stencil(img_path: str, text: str, text_size=75, text_coords=(50, 50)) -> np.array:
     """
     Encodes the text into the image using a stencil approach.
     """
+
+    print("encode_stencil")
     
+    img = Image.open(img_path).convert("RGBA")
+    img_data = np.asarray(img)
+
+    text_img = Image.new("RGB", (500, 100), color="white")
+    # fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", text_size)
+    draw = ImageDraw.Draw(text_img)
+    draw.text((0, 0), text, fill=(0, 255, 0))
+    # text_img.show()
+
+    key = generate_key()
+
+    for y in range(100):
+        for x in range(500):
+            if (text_img.getpixel((x, y)) == (0, 255, 0)):
+                img_data[y][x] = encode_pixel(img_data[y][x], key)
+
+    return img_data
