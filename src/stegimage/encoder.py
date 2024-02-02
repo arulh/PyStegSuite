12b52e4452009241167cdbdcd7c5551fbea8811a
arulh <arul.howard@telus.net>
2024-02-01 22:57:27 -0800

    added font size argument for encode_stencil

--- src/stegimage/encoder.py
+++ src/stegimage/encoder.py
@@ -3,26 +3,27 @@ import numpy as np
 
 from helper import encode_pixel, generate_key
 
-def encode_stencil(img_path: str, text: str, text_size=75, text_coords=(50, 50)) -> np.array:
+def encode_stencil(img_path: str, encoded_text: str, text_size=75, text_coords=(50, 50)) -> np.array:
     """
     Encodes the text into the image using a stencil approach.
     """
-
-    print("encode_stencil")
     
     img = Image.open(img_path).convert("RGBA")
-    img_data = np.asarray(img)
+    img_data = np.array(img)
 
-    text_img = Image.new("RGB", (500, 100), color="white")
-    # fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", text_size)
+    fnt = ImageFont.load_default(text_size)
+    text_length = int(fnt.getlength(encoded_text))
+
+    text_img = Image.new("RGB", (text_length, text_size), color="white")
+    
     draw = ImageDraw.Draw(text_img)
-    draw.text((0, 0), text, fill=(0, 255, 0))
-    # text_img.show()
+    
+    draw.text((0, 0), text=encoded_text, fill=(0, 255, 0), font=fnt)
 
     key = generate_key()
 
-    for y in range(100):
-        for x in range(500):
+    for y in range(text_size):
+        for x in range(text_length):
             if (text_img.getpixel((x, y)) == (0, 255, 0)):
                 img_data[y][x] = encode_pixel(img_data[y][x], key)
 
