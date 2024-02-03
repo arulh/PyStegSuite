4173000f68511feafdb1d84fd88dfdcb71d054e0
arulh <arul.howard@telus.net>
2024-02-03 13:34:45 -0800

    implemented decode_stencil

---
+++ src/stegimage/decoder.py
@@ -0,0 +1,21 @@
+import numpy as np
+from PIL import Image
+
+from helper import is_encoded_pixel
+
+def decode_stencil(img_path: str, key: int) -> Image.Image:
+    """
+    Decodes image for encrypted stencil.
+    """
+
+    img = Image.open(img_path).convert("RGBA")
+
+    length, height = img.getbbox()[2]-img.getbbox()[0], img.getbbox()[3]-img.getbbox()[1]
+
+    for y in range(height):
+        for x in range(length):
+            if (is_encoded_pixel(img.getpixel((x, y)), key)):
+                # sets decrypted pixels to green
+                img.putpixel((x, y), (0, 255, 0, 255))
+
+    return img
--- src/stegimage/encoder.py
+++ src/stegimage/encoder.py
@@ -3,18 +3,21 @@ import numpy as np
 
 from helper import encode_pixel, generate_key
 
-def encode_stencil(img_path: str, encoded_text: str, text_size=75, text_coords=(50, 50)) -> np.array:
+def encode_stencil(img_path: str, encoded_text: str, text_size=75, text_coords=(50, 50)) -> tuple:
     """
     Encodes the text into the image using a stencil approach.
+
+    Returns:
+        Image object, key
     """
     
     img = Image.open(img_path).convert("RGBA")
-    img_data = np.array(img)
+    length, height = img.getbbox()[2]-img.getbbox()[0], img.getbbox()[3]-img.getbbox()[1]
 
     # check valid arguments
-    if (text_size > img_data.shape[1]):
+    if (text_size > height):
         raise ValueError("TEXT SIZE MUST FIT ON IMAGE")
-    elif (text_coords[0] > img_data.shape[0] or text_coords[1] > img_data.shape[1] or text_coords[0] < 0 or text_coords[1] < 0):
+    elif (text_coords[0] > length or text_coords[1] > height or text_coords[0] < 0 or text_coords[1] < 0):
         raise ValueError("TEXT COORDINATES NEED TO BE IN IMAGE")
     elif (encoded_text.isspace()):
         raise ValueError("ENCODED TEXT MUST CONTAIN AT LEAST ONE CHARACTER")
@@ -33,6 +36,6 @@ def encode_stencil(img_path: str, encoded_text: str, text_size=75, text_coords=(
     for y in range(text_size):
         for x in range(text_length):
             if (text_img.getpixel((x, y)) == (0, 255, 0)):
-                img_data[y][x] = encode_pixel(img_data[y][x], key)
+                img.putpixel((x, y), encode_pixel(img.getpixel((x, y)), key))
 
-    return img_data
+    return img, key
